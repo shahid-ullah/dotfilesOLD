e089955051cc9fb89a795583a3e2cbb393d500b6
shahid <shahid.ice.nstu@gmail.com>
2021-05-26 13:26:19 +0600

    index init.lua

---
+++ init.lua
@@ -0,0 +1,11 @@
+require'lspconfig'.pyright.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.pylsp.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.pyls.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.tsserver.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.bashls.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.cssls.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.denols.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.html.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.tsserver.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.vimls.setup{on_attach=require'completion'.on_attach}
+-- require('tools')
